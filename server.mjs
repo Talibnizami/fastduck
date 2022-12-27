cb5b9c866ccfc87a6e6e63deee4a481433ef6669
Talibnizami <talibnizamim@gmail.com>
2022-12-27 21:47:20 +0500

    update

--- server.mjs
+++ server.mjs
@@ -176,9 +176,7 @@ app.get("/historyCustomer", (req, res) => {
 
 
 
-const __dirname = path.resolve();
-app.use('/', express.static(path.join(__dirname, './web/build')))
-app.use('*', express.static(path.join(__dirname, './web/build')))
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
